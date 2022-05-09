f92cd007feb3bf022f8cb1c86bf4e9a86768f646
Onda-dev <manuelaltieri3@gmail.com>
2022-05-09 16:09:02 +0200

    Prova seconda parte

--- main.js
+++ main.js
@@ -7,19 +7,21 @@ while ( randomNumbers.length < 16 ) {
         randomNumbers.push(number)
     }
 }
-console.log(randomNumbers)
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 const userChoice = []
-while ( userChoice.length < 4 ) {
-    let number = Number(prompt("Inserisci un numero"))
-    if ( !userChoice.includes(number)) {
-        userChoice.push(number)
+while ( randomNumbers.length <= 24 ) {
+    let number = Number(prompt("Inserisci un numero compreso tra 1 e 100"))
+    userChoice.push(number)
+    if ( !randomNumbers.includes(number) ) {
+        randomNumbers.push(number)
     } else {
-        alert("Numero già inserito, scegline un altro")
+        alert ("Hai perso!")
     }
 }
 console.log(userChoice)
+console.log(randomNumbers)
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
