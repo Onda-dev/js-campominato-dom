a66256e4648bdcf89e5fda835358f0eb530acbe8
Onda-dev <manuelaltieri3@gmail.com>
2022-05-09 17:12:46 +0200

    fix

--- main.js
+++ main.js
@@ -10,11 +10,15 @@ while ( bombsList.length < 16 ) {
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 let userChoices = []
-do {
-    userNumber = Number(prompt("Inserisci un numero compreso tra 1 e 100"))
-    userChoices.push(userNumber)
-} while ( isNaN(userNumber > 100 || userNumber < 1) )
 
+while (userChoices <= 22) {
+
+    do {
+        userNumber = Number(prompt("Inserisci un numero compreso tra 1 e 100"))
+        userChoices.push(userNumber)
+    } while ( isNaN(userNumber > 100 || userNumber < 1) || !userChoices.includes(userNumber) )
+
+}
 console.log(userChoices)
 console.log(bombsList)
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
