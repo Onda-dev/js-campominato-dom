abd63899a317a85d5e9b5f02f1fc75608d38448f
Onda-dev <manuelaltieri3@gmail.com>
2022-05-11 11:59:31 +0200

    Inizio parte grafica

---
+++ js/mainWithDom.js
@@ -0,0 +1,69 @@
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 => tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+// function randomNumber(min, max) {
+//     return Math.floor(Math.random() * (max - min +1) ) + min
+// }
+
+// let level
+// do {
+//     level = Number(prompt("Inserisci un livello di difficoltà fra 0, 1 e 2"))
+// } while ( isNaN(level) || level > 2 || level < 0)
+
+// let totalNumbers
+// if ( level === 0 ) {
+//     totalNumbers = 100
+// } else if ( level === 1 ) {
+//     totalNumbers = 80
+// } else {
+//     totalNumbers = 50
+// }
+
+// console.log(totalNumbers)
+// const bombsList = []
+// const boombsNumber = 16
+// const attemptsNumber = totalNumbers - boombsNumber
+
+// while ( bombsList.length < boombsNumber ) {
+//     const bombs = randomNumber(1, totalNumbers)
+
+//     if (!bombsList.includes(bombs) ) {
+//         bombsList.push(bombs)
+//     }
+// }
+// console.log(bombsList)
+// // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// // L’utente non può inserire più volte lo stesso numero.
+// // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// let userAttempts = []
+// let bombExploded = false
+
+// while ( !bombExploded && userAttempts.length < attemptsNumber ) {
+
+//     let userNumber
+//     do {
+//         userNumber = Number(prompt(`Inserisci un numero compreso tra 1 e ${totalNumbers}`))
+//     } while ( isNaN(userNumber) || userNumber < 1 || userNumber > totalNumbers )
+
+//     if ( bombsList.includes(userNumber) ) {
+//         bombExploded = true
+//     } else if ( userAttempts.includes(userNumber) ) {
+//         alert("Il numero è già stato inserito")
+//     } else {
+//         userAttempts.push(userNumber)
+//     }
+    
+// }
+// // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito
+// if ( bombExploded ) {
+//     alert (`Hai perso! Il tuo punteggio è ${userAttempts.length}`)
+// } else {
+//     alert ("Hai vinto!")
+// }
+// console.log("Gioco terminato")
+
+
+
+
